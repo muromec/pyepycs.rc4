2e2c887e41da9a5fac2af2377b923627040c5175
Ilya Petrov <ilya.muromec@gmail.com>
2012-05-02 21:05:32 +0700

    fix packaging

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup, Extension
 
 setup(name="epycs.rc4", version="0.1",
+      packages = ['epycs'],
       ext_modules=[
          Extension("epycs.rc4", 
              sources=[
-		"rc4module.c",
-		"crc32_util.c",
-		"skype_rc4.c",	
+		"epycs/rc4module.c",
+		"epycs/crc32_util.c",
+		"epycs/skype_rc4.c",	
 	     ]
          ),
          ])
